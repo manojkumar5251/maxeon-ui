3f818832557875908fcb4aaad4fbda0505688543
Manoj Kumar <manojkumar5251@gmail.com>
2020-07-24 16:12:50 +0530

    Updates

---
+++ precache-manifest.b1c688f57ef00ae65c703c3c11ab5496.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1064ad6e6788ef584f5791742396162e",
+    "url": "/maxeon-ui/index.html"
+  },
+  {
+    "revision": "76a1927512323554b71e",
+    "url": "/maxeon-ui/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1d1bbda7ce532718ed00",
+    "url": "/maxeon-ui/static/css/main.611a3955.chunk.css"
+  },
+  {
+    "revision": "76a1927512323554b71e",
+    "url": "/maxeon-ui/static/js/2.2f3d825d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/maxeon-ui/static/js/2.2f3d825d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d1bbda7ce532718ed00",
+    "url": "/maxeon-ui/static/js/main.d3af8df2.chunk.js"
+  },
+  {
+    "revision": "6885e9f1845d532df86c",
+    "url": "/maxeon-ui/static/js/runtime-main.20c43494.js"
+  },
+  {
+    "revision": "4a6ebd5ac6c39df826439199c960c8ef",
+    "url": "/maxeon-ui/static/media/Banner.4a6ebd5a.png"
+  }
+]);
